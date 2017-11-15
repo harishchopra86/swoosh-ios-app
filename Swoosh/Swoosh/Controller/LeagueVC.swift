f7348e533fffcfdbc53bfb350d69e7762684885b
harish <harishchopra86@gmail.com>
2017-11-15 10:42:06 -0500

    new struct added for passing data between screens data passed between screens through struct minor ui changes

--- Swoosh/Swoosh/Controller/LeagueVC.swift
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -10,26 +10,45 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    @IBOutlet weak var btnNext: BorderButton!
+    var player:Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btnMensTapped(_ sender: Any) {
+        selectLeaque(leagueType: "mens")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnWomenTapped(_ sender: Any) {
+        selectLeaque(leagueType: "womens")
     }
-    */
-
+    
+    @IBAction func btnCoedTapped(_ sender: Any) {
+        selectLeaque(leagueType: "coed")
+    }
+    
+    func selectLeaque(leagueType:String) {
+        player.desiredLeague = leagueType
+        btnNext.isEnabled = true
+    }
+    
+    @IBAction func btnNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
 }
---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Harish Chopra on 2017-11-15.
+//  Copyright © 2017 Harish Chopra. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+   
+    var desiredLeague:String?
+    var selectedSkillLevel:String?
+    
+    
+}
